0056d4ce1e1fcff968ae61cceb1314e4693d0d7e
ujwalg63 <144614933+ujwalg63@users.noreply.github.com>
2026-01-15 18:59:49 -0500

    Time: 0 ms (100.00%) | Memory: 51.1 MB (55.48%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int uniqueCount = 1;
+        for(int i=1, j=0; i<=nums.Length-1; i++)
+        {
+            if(nums[j] != nums[i])
+            {
+                j=j+1;
+                nums[j] = nums[i];
+                uniqueCount = uniqueCount+1;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return uniqueCount;
+    }
+}
