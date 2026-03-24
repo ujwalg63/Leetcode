ba11dba99390c6f28467e206be643368ed9aa29a
ujwalg63 <144614933+ujwalg63@users.noreply.github.com>
2026-03-24 12:13:18 -0400

    Update TopKfrequentElements.cs

--- TopKfrequentElements.cs
+++ TopKfrequentElements.cs
@@ -15,6 +15,21 @@ public class Solution {
         }
         var sortedList = map.ToList();
         sortedList.Sort((pair1, pair2) => pair2.Value.CompareTo(pair1.Value));
+        //Let's say the values of pairs are equal, then we want to sort by keys in ascending order. Then, we would do this,
+        sortedList.Sort((pair1, pair2) => 
+        // {
+        //     // First, compare by Value (Descending)
+        //     int result = pair2.Value.CompareTo(pair1.Value);
+            
+        //     // If Values are the same (result is 0), compare by Key (Ascending)
+        //     if (result == 0)
+        //     {
+        //         return pair1.Key.CompareTo(pair2.Key);
+        //     }
+            
+        //     return result;
+        // });
+        
         return sortedList.GetRange(0, k).Select(pair => pair.Key).ToArray();
     }
 }
